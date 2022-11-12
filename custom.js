657762c8b3b50533d3a86ff6da1e7836943bb249
Adriano Orlando <adrianoorlando04@gmail.com>
2022-11-12 17:01:57 +1300

    Add files via upload

--- custom.js
+++ custom.js
@@ -24,6 +24,48 @@ $('body').on('click', '#ht-summerboy-toggle', function(e) {
   $(".ht-summerboy-video-container").toggleClass("ht-summerboy-video-container--show");
 });
 
+//Hover images
+
+$('body').on('mouseover', '#ht-language-toggle', function(e) {
+  e.preventDefault();
+  $(".ht-hover-image--language").toggleClass("ht-hover-image--show");
+});
+
+$('body').on('mouseleave', '#ht-language-toggle', function(e) {
+  e.preventDefault();
+  $(".ht-hover-image--language").removeClass("ht-hover-image--show");
+});
+
+$('body').on('mouseover', '#ht-billboard-toggle', function(e) {
+  e.preventDefault();
+  $(".ht-hover-image--billboard").toggleClass("ht-hover-image--show");
+});
+
+$('body').on('mouseleave', '#ht-billboard-toggle', function(e) {
+  e.preventDefault();
+  $(".ht-hover-image--billboard").removeClass("ht-hover-image--show");
+});
+
+$('body').on('mouseover', '#ht-sign-toggle', function(e) {
+  e.preventDefault();
+  $(".ht-hover-image--sign").toggleClass("ht-hover-image--show");
+});
+
+$('body').on('mouseleave', '#ht-sign-toggle', function(e) {
+  e.preventDefault();
+  $(".ht-hover-image--sign").removeClass("ht-hover-image--show");
+});
+
+$('body').on('mouseover', '#ht-road-toggle', function(e) {
+  e.preventDefault();
+  $(".ht-hover-image--road").toggleClass("ht-hover-image--show");
+});
+
+$('body').on('mouseleave', '#ht-road-toggle', function(e) {
+  e.preventDefault();
+  $(".ht-hover-image--road").removeClass("ht-hover-image--show");
+});
+
 // Video player
 
 function playVideo() {
